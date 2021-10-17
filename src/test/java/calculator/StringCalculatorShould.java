a3e671a30af4ea21463192e28b5da08cb07fcb40
SACHIN RANE <85445714+sachinrane1311@users.noreply.github.com>
2021-10-17 22:08:26 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,75 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	// with space
+	@Test
+	void testEmptyString() {
+		int result = StringCalculator.Add(" ");
+		assertTrue(result == 0);
+	}
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	// with two numbers
+	@Test
+	void testStringwithTwoNumber() {
+		int result = StringCalculator.Add("1,2");
+		assertTrue(result == 3);
+	}
+
+	// with any amount of numbers
+	@Test
+	public final void whenAnyAmountOfNumbers() {
+		assertEquals(3 + 6 + 15 + 18 + 46 + 33, StringCalculator.Add("3,6,15,18,46,33"));
+	}
+
+	// with newLine and comma
+	@Test
+	void testStringwithNewLine() {
+		int result = StringCalculator.Add("1,2, ,\n,3");
+		assertTrue(result == 6);
+	}
+
+	// with different delimiters
+	@Test
+	void testStringwithMixedDelimiter() {
+		int result = StringCalculator.Add("1,2, ,\n,3,.1;7:8|7/6//6");
+		assertTrue(result == 41);
+	}
+
+	// with Negative Number
+	@Test
+	void testStringwithNegative() {
+		int result = StringCalculator.Add("1,2,\n,3,-5,7");
+		assertTrue(result == 0);
+	}
+
+	// with greater than 1000 Number
+	@Test
+	void testStringwithGreaterThanThousand() {
+		int result = StringCalculator.Add("1,2,1000");
+		assertTrue(result == 3);
+	}
+
+	//with any length delimiter
+	@Test
+	void testStringDelimeterCanbeofanylengthDelimiter() {
+		int result = StringCalculator.Add("[*]\n1**2*3", "[]\n*");
+		assertTrue(result == 6);
+
+	}
+
+	//with multiple delimiter
+	@Test
+	void testStringMultipleDelimiter() {
+		int result = StringCalculator.Add("//[][%]\n1*2%3", "/%[]\n*");
+		assertTrue(result == 6);
+
+	}
+
+	//with multiple delimiter length
+	@Test
+	void testStringMultipleDelimiterLengthLongerThanOneChar() {
+		int result = StringCalculator.Add("1delim2*3", "delim*");
+		assertTrue(result == 6);
+
+	}
 }
